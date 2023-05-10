cfdc116adf8803d2d4acfee7f05abd76740957cc
Rodrigo <r.barboza9502@gmail.com>
2023-05-10 10:43:32 -0300

    Domain model

---
+++ src/main/java/com/rodrigo/dslist/entities/Belonging.java
@@ -0,0 +1,68 @@
+package com.rodrigo.dslist.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_belonging")
+public class Belonging implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@EmbeddedId
+	private BelongingPK id = new BelongingPK();
+	
+	private Integer position;
+	
+	public Belonging() {
+	}
+
+	public Belonging(Game game, GameList list, Integer position) {
+		id.setGame(game);
+		id.setList(list);
+		this.position = position;
+	}
+
+	public BelongingPK getId() {
+		return id;
+	}
+
+	public void setId(BelongingPK id) {
+		this.id = id;
+	}
+
+	public Integer getPosition() {
+		return position;
+	}
+
+	public void setPosition(Integer position) {
+		this.position = position;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Belonging other = (Belonging) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
