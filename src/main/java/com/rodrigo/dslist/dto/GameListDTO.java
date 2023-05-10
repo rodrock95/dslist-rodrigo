e9deee91ca12e536034177188e60e4d2b53d7dce
Rodrigo <r.barboza9502@gmail.com>
2023-05-10 11:32:40 -0300

    GET lists

---
+++ src/main/java/com/rodrigo/dslist/dto/GameListDTO.java
@@ -0,0 +1,25 @@
+package com.rodrigo.dslist.dto;
+
+import com.rodrigo.dslist.entities.GameList;
+
+public class GameListDTO {
+
+	private Long id;
+	private String name;
+	
+	public GameListDTO() {
+	}
+	
+	public GameListDTO(GameList entity) {
+		id = entity.getId();
+		name = entity.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
---
+++ src/main/java/com/rodrigo/dslist/repositories/GameListRepository.java
@@ -0,0 +1,9 @@
+package com.rodrigo.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rodrigo.dslist.entities.GameList;
+
+public interface GameListRepository extends JpaRepository<GameList, Long> {
+
+}
